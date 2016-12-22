89ee14c4090fe260572039e496aac01f07a570e9
MichaelWiss <michael.wiss@gmail.com>
2016-12-21 18:44:34 -0600

    added axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
 const API_KEY = 'efdd8131247971037e8b3299672be35a';
+const root_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;                      
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+esport function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+
+	return {
+		type: FETCH_WEATHER
+	}
+}
